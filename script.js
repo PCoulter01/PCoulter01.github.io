07e6a4be40cf2fd02ac86aa98c17d36925064fbd
Patrick Coulter <86530430+Patepic@users.noreply.github.com>
2021-07-18 09:16:15 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.Navbar__Link-toggle').addEventListener('click', classToggle);
-
-function classToggle() {
-  const navs = document.querySelectorAll('.Navbar__Items')
-  
-  navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
-}
